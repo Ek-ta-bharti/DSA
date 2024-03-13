fc56d4ca4ec496f456642346e556b0e6b71111c1
Ekta bharti <ektabharti235@gmail.com>
2024-03-13 05:49:57 +0530

    13-03

---
+++ Days/Day2/UserRemoveElement.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class UserRemoveElement {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Enter Element Number: ");
+        
+    }
+    
+}
