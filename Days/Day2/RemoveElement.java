f52677ab023e95a18ed752d8d1f9ba1c6bf02767
Ekta bharti <ektabharti235@gmail.com>
2024-03-13 05:46:34 +0530

    13-03

--- Days/Day2/RemoveElement.java
+++ Days/Day2/RemoveElement.java
@@ -1,3 +1,22 @@
 public class RemoveElement {
-    
+    public static int duplicateElement(int[] nums, int val) {
+        int trg = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[trg] = nums[i];
+                trg++;
+            }
+        }
+        return trg;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 4, 6, 3, 2, 6, 6 };
+        int val = 3;
+       
+        int newSize = duplicateElement(nums, val);
+        for (int i = 0; i < newSize; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
