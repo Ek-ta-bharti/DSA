550962d7526f87814f46c0592e7bbea1b87b9633
Ekta bharti <ektabharti235@gmail.com>
2024-03-14 06:43:09 +0530

    14-03

---
+++ Days/Day3/SquareOfShortedArray.java
@@ -0,0 +1,15 @@
+package Days.Day3;
+
+public class SquareOfShortedArray {
+    public static int SquareExample(int arr[], int n) {
+
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        int arr[]= { 5 ,9, 3, 2, 8, 9};
+         int new = SquareExample ();
+        System.out.println();
+    }
+
+}
