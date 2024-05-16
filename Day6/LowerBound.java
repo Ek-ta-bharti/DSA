efafb3fa7c50fa403a788ed375fc8007eaf8f947
Ekta bharti <ektabharti235@gmail.com>
2024-05-16 22:53:55 +0530

    16

---
+++ Day6/LowerBound.java
@@ -0,0 +1,12 @@
+package Day6;
+import java.util.*;
+import java.io.*;
+
+public class LowerBound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of element: ");
+        int n =sc.nextInt();
+    }
+    
+}
