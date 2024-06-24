4a71f6a4f1d0ed9c1cbe0d4ec2da9729d4d827c5
Ekta bharti <ektabharti235@gmail.com>
2024-06-24 18:48:18 +0530

    24

--- Day7/Fibonacci.java
+++ Day7/Fibonacci.java
@@ -1,12 +1,27 @@
 package Day7;
+
 import java.util.*;
 import java.io.*;
 
 public class Fibonacci {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter Number for Fibonacci>> ");
-        
+    public static int Fs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return Fs(n - 1) + Fs(n - 2);
     }
-    
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number for Fibonacci>> ");
+        int n = sc.nextInt();
+        sc.close();
+
+        int sum = Fs(n);
+        System.out.print(sum);
+    }
+
 }
