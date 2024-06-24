4d1f08ba03e3647840f69f24182869feebf67637
Ekta bharti <ektabharti235@gmail.com>
2024-06-24 18:23:24 +0530

    24

---
+++ Day7/Fibonacci.java
@@ -0,0 +1,12 @@
+package Day7;
+import java.util.*;
+import java.io.*;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Number for Fibonacci>> ");
+        
+    }
+    
+}
