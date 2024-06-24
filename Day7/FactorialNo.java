ef3a89c13888880962e7f1b14a281e250e07afc3
Ekta bharti <ektabharti235@gmail.com>
2024-06-24 18:15:01 +0530

    24

--- Day7/FactorialNo.java
+++ Day7/FactorialNo.java
@@ -1,7 +1,23 @@
 package Day7;
+
 import java.util.*;
 import java.io.*;
 
 public class FactorialNo {
-    
+
+    public static int Factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        return n * Factorial(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number For Factorial >> ");
+        int n = sc.nextInt();
+
+        int sum = Factorial(n);
+        System.out.print("Factorial " + n +  " Result>> "+ sum);
+    }
 }
