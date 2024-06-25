ab2cb1b248c0e8de6247ace8747740d77e7cdd50
Ekta bharti <ektabharti235@gmail.com>
2024-06-25 10:37:15 +0530

    25

--- Day7/BinarySearch.java
+++ Day7/BinarySearch.java
@@ -1,5 +1,28 @@
 package Day7;
 
+import java.util.*;
+
 public class BinarySearch {
-    
+
+    public static int Bs(int arr[], int searchelement, int low, int high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == searchelement) {
+            return mid;
+        }
+        if (arr[mid] > searchelement) {
+            return Bs(arr, searchelement, low, mid - 1);
+        }
+        return Bs(arr, searchelement, high, mid + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 5, 8, 12, 67, 98, 666 };
+        int searchelement = 98;
+
+        int resul = Bs(arr, searchelement, 0, arr.length - 1);
+
+        System.out.println(resul);
+    }
+
 }
