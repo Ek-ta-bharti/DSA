cf3b95d7ea1c4b37725b64f17a7274b73bcd200d
Ekta bharti <ektabharti235@gmail.com>
2024-06-26 15:43:26 +0530

    26

--- Day8/LinkedEnd.java
+++ Day8/LinkedEnd.java
@@ -3,7 +3,7 @@ public class LinkedEnd {
     class Node{
         int data;
         Node next;
-
+// Constructor to create a new node
         Node(int d){
             data=d;
             next =null;
