f2c5f4add07cf6d40c21d3ba980fb9bcedaa769d
Ekta bharti <ektabharti235@gmail.com>
2024-06-26 22:03:08 +0530

    26

--- Day8/LinkedBeginning.java
+++ Day8/LinkedBeginning.java
@@ -11,21 +11,47 @@ public class LinkedBeginning {
         }
     }
 
-    public void LinkedEnd(int newData) {
-        Node newNode = new Node(newData);
-        while (head == null) {
-            head = new Node(newData);
-            return;
+    // public void InsertAtFirst(int newData) {
+    //     Node newNode = new Node(newData);
+    //     while (head == null) {
+    //         head = new Node(newData);
+    //         return;
+    //     }
+    //     newNode.next = null;
+    //     Node temp = head;
+    //     while(temp.next !=  null){
+    //          temp = temp.next;
+    //     }
+    //     temp.next = newNode;
+    //     return;
+    // }
+    public void InsertAtFirst( int newData){
+     Node newNode = new Node(newData);
+     newNode.next =head;
+     head = newNode;
+    }
+    public void display(){
+        Node current = head;
+        while (current != null) {
+            System.out.print("---> "  +current.data);
+            current = current.next;
         }
-        newNode.next = null;
-        Node temp = head;
-        while(temp.next !=  null){
-            
-        }
-
     }
 
     public static void main(String[] args) {
+        LinkedBeginning  list = new LinkedBeginning ();
+        list.InsertAtFirst(2);
+        list.InsertAtFirst(9);
+        list.InsertAtFirst(10);
 
+        System.out.println("Before insertion of 10");
+        list.display();
+        System.out.println();
+
+        list.InsertAtFirst(79);
+        System.out.println("After insertion of 10");
+        list.display();
+        System.out.println();
     }
-}
+    }
+
