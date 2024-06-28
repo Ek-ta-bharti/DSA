68c3816d0b32d27c4f6ba22068f2961e5e4b71c1
Ekta bharti <ektabharti235@gmail.com>
2024-06-28 06:59:07 +0530

    28

--- Day8/ReversalLL.java
+++ Day8/ReversalLL.java
@@ -1,4 +1,23 @@
 public class ReversalLL {
-    
-    
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void InsertAtFirst
+    {
+
+    }
+
+    public static void main(String[] args) {
+        ReversalLL List = new ReversalLL();
+    }
+
 }
