6285560d5d9d715055e50ddc0f82369a168fba06
Ekta bharti <ektabharti235@gmail.com>
2024-07-03 06:15:13 +0530

    03

--- Day11/SequenceString.java
+++ Day11/SequenceString.java
@@ -1,10 +1,24 @@
 package Day11;
 
+import java.util.*;
+
 public class SequenceString {
-    public static void function(int n){
-        
+    public static void function(int[] arr, int index, ArrayList<Integer> tempArr) {
+        if (index == arr.length) {
+            if (tempArr.size() > 0) {
+                System.out.println(tempArr);
+            }
+            return;
+        }
+        function(arr, index + 1, tempArr); // Recursive call without including current element
+        tempArr.add(arr[index]);
+        function(arr, index + 1, tempArr); // Recursive call including current element
+        tempArr.remove(tempArr.size() - 1); // Backtrack
     }
+
     public static void main(String[] args) {
-        
+        int[] arr = { 1, 2 };
+        System.out.println("For the Array " + Arrays.toString(arr));
+        function(arr, 0, new ArrayList<>());
     }
 }
